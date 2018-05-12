9c6112a3f581ba8294522ac04ddd932ffee1f928
Edward Pfremmer <epfremmer@gmail.com>
2018-05-12 16:04:29 -0500

    Added todo list component

---
+++ src/todoList.tsx
@@ -0,0 +1,22 @@
+/// <reference path="./interfaces.d.ts"/>
+
+import * as React from 'react';
+import { TodoItem } from "@app/todoItem";
+
+interface TodoListProps {
+  todos: ITodo[];
+}
+
+export class TodoList extends React.Component<TodoListProps> {
+  render() {
+    const { todos } = this.props;
+
+    return (
+      <React.Fragment>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
+      </React.Fragment>
+    );
+  }
+}
