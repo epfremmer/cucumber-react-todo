bc48a80c29fdcd34f353f50c975ce585328832fb
Edward Pfremmer <epfremmer@gmail.com>
2018-05-18 00:41:12 -0500

    Converted application to use redux router

--- src/footer.tsx
+++ src/footer.tsx
@@ -4,14 +4,15 @@ import * as React from 'react';
 import * as classNames from 'classnames';
 import * as pluralize from 'pluralize';
 import { Link } from "react-router-dom";
+import { RouterState } from "react-router-redux";
 import { connect } from "react-redux";
 
-import { ALL_TODOS, ACTIVE_TODOS, COMPLETED_TODOS } from "./constants";
 import { clearCompleted } from "@app/actions";
 
 interface ConnectedStateProps {
   activeCount: number;
   completedCount: number;
+  location: RouterState['location'];
 }
 
 interface ConnectedDispatchProps {
@@ -25,9 +26,8 @@ class TodoFooterComponent extends React.Component<TodoFooterComponentProps> {
   private clearCompleted = () => this.props.clearCompleted();
 
   public render() {
-    const { activeCount, completedCount } = this.props;
-    // @todo: change this out with redux location data
-    const nowShowing = window.location.hash.replace('#/', '');
+    const { activeCount, completedCount, location } = this.props;
+    const nowShowing = location ? location.pathname : null;
 
     return (
       <footer className="footer">
@@ -37,15 +37,15 @@ class TodoFooterComponent extends React.Component<TodoFooterComponentProps> {
         </span>
         <ul className="filters">
           <li>
-            <Link to="/" className={classNames({selected: nowShowing === ALL_TODOS})}>All</Link>
+            <Link to="/" className={classNames({selected: nowShowing === '/'})}>All</Link>
           </li>
           <span>{' '}</span>
           <li>
-            <Link to="/active" className={classNames({selected: nowShowing === ACTIVE_TODOS})}>Active</Link>
+            <Link to="/active" className={classNames({selected: nowShowing === '/active'})}>Active</Link>
           </li>
           <span>{' '}</span>
           <li>
-            <Link to="/completed" className={classNames({selected: nowShowing === COMPLETED_TODOS})}>Completed</Link>
+            <Link to="/completed" className={classNames({selected: nowShowing === '/completed'})}>Completed</Link>
           </li>
         </ul>
         {completedCount > 0 &&
@@ -58,13 +58,14 @@ class TodoFooterComponent extends React.Component<TodoFooterComponentProps> {
   }
 }
 
-const mapStateToProps = ({ todos }) => ({
+const mapStateToProps = ({ todos, routing }) => ({
   activeCount: todos.filter(todo => !todo.completed).length,
-  completedCount: todos.filter(todo => todo.completed).length
+  completedCount: todos.filter(todo => todo.completed).length,
+  location: routing.location
 });
 
 const mapDispatchToProps = {
-  clearCompleted
+  clearCompleted,
 };
 
 export const TodoFooter = connect(mapStateToProps, mapDispatchToProps)(TodoFooterComponent);
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom'
-import { store } from "@app/store";
-import { Provider } from "react-redux";
+import * as ReactDOM from 'react-dom'
+import { Provider } from "react-redux";;
+import { ConnectedRouter } from "react-router-redux";
+import { store, history } from "@app/store";
 import { TodoApp } from "@app/app";
+
+import 'todomvc-common/base.css';
+import 'todomvc-app-css/index.css';
 
 ReactDOM.render((
     <Provider store={store}>
-      <HashRouter>
+      <ConnectedRouter history={history}>
         <TodoApp />
-      </HashRouter>
+      </ConnectedRouter>
     </Provider>
   ),
   document.getElementsByClassName('todoapp')[0]
